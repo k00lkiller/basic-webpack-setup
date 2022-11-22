3d9d933ee68930b44e74ea416cbd28e8d1e6c9bf
Christine Brovkina <cbrowkina@hfk-bremen.de>
2022-11-22 14:57:28 +0100

    add webpack-dev-server and configure hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const HTMLWebpackPlugin = require ('html-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'script.js',
-      path: path.resolve(__dirname, "build"),
-      publicPath: "./",
-      assetModuleFilename: "images/[name][ext][query]"
+	entry: './src/index.js',
+	output: {
+		filename: 'script.js',
+		path: path.resolve(__dirname, 'build'),
+		assetModuleFilename: 'images/[name][ext][query]'
+	},
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    hot: true,
+    compress: true,
+    port: 9000,
   },
-  plugins:[
-    new HTMLWebpackPlugin(
-      {
-        template: './src/index.html'
-      }
-    ),
-    new MiniCSSExtractPlugin(),
-  ],
-  optimization: {
-    minimizer: [
-      new CSSMinimizerPlugin(),
-    ],
-  },
-  
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          MiniCSSExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  }
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new MiniCSSExtractPlugin()
+	],
+	optimization: {
+		minimizer: [new CSSMinimizerPlugin()]
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/,
+				use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: 'asset/resource'
+			}
+		]
+	}
 }
-  
